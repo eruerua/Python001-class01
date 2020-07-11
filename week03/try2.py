9018f1febad45237cd8d2ad217c6684701bfb463
houhiroshi <eruerame@gmail.com>
2020-07-11 20:40:21 +0800

    add sth

--- week03/try2.py
+++ week03/try2.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests 
 import time
 headers = { 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 
@@ -14,38 +15,6 @@ def Positon(city,page):
     rep = session.post('https://www.lagou.com/jobs/positionAjax.json?city='+city+'&needAddtionalResult=false',headers=headers,data=data) # 请求成功，接下来就是提取想要的信息。 
     result=rep.json()['content']['positionResult']['result']
     return result
-
-def callback(future):
-    global result
-    print('抓取成功')
-    result.append(future.result)
-# print(len(Positon('北京',1)))
-# time.sleep(2)
-# print(len(Positon('北京',2)))
-# time.sleep(2)
-# print(len(Positon('北京',3)))
-# time.sleep(2)
-# print(len(Positon('北京',4)))
-# time.sleep(2)
-# print(len(Positon('北京',5)))
-# time.sleep(2)
-# print(len(Positon('北京',6)))
-# time.sleep(2)
-# print(len(Positon('北京',7)))
-# time.sleep(2)
-from concurrent.futures import ThreadPoolExecutor
-import time
-citys=['北京','上海','广州','深圳']
-result = []
-with ThreadPoolExecutor(3) as executor:
-    for i in range(1,8):
-        for c in citys:
-            try:
-                r=executor.submit(Positon,c,i)
-                r.add_done_callback(callback)
-                
-            except Exception as e:
-                print('抓取失败')
-                print(e)
-            time.sleep(2)
-print(len(result))
+l=Positon('北京',1)
+df=pd.read_json(l,encoding="utf-8")
+print(df.head())
